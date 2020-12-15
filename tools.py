10bbdc7f38471765f50adf1fc211f2e89b909622
caustic <42774254+CausticEngineer@users.noreply.github.com>
2020-12-15 13:02:03 +0300

    add import and load_elements function

--- tools.py
+++ tools.py
@@ -1,3 +1,14 @@
+import json
+import cv2
+import numpy as np
+
+
+def load_elements(json_file):
+    # load file and extract massive of elements with its features
+    # example [['H', 'Hydrogen', 1, 1],['He', 'Helium', 2, 4]]
+    pass
+
+
 def find_bb_in_name(name):
     # finding chemical elements in a given single word
     # return dict of possible variants, example {'H': 4, 'Ti': 0}
