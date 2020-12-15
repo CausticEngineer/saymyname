03238164962802ed8936d1611deee84202fbce35
caustic <42774254+CausticEngineer@users.noreply.github.com>
2020-12-15 12:48:03 +0300

    add base functions plugs in tools (find_bb_in_name, choose_best)

---
+++ tools.py
@@ -0,0 +1,10 @@
+def find_bb_in_name(name):
+    # finding chemical elements in a given single word
+    # return dict of possible variants, example {'H': 4, 'Ti': 0}
+    pass
+
+
+def choose_best(vars_dict):
+    # choosing the best looking element and it`s position
+    # return a tuple, example ('Ti', 0)
+    pass
