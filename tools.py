b607ae82f25c325bc4fa48f7b0ff875b5b7263e0
caustic <42774254+CausticEngineer@users.noreply.github.com>
2020-12-15 17:26:03 +0300

    completed find_bb_in_name

--- tools.py
+++ tools.py
@@ -13,8 +13,15 @@ def load_elements(json_file):
 
 def find_bb_in_name(name):
     # finding chemical elements in a given single word
-    # return dict of possible variants, example {'H': 4, 'Ti': 0}
-    pass
+    # return dict of possible variants
+    # example [[0, 'Ti'], [1, 'I'], [2, 'K'], [3, 'H'], [3, 'Ho'], [4, 'O'], [5, 'N'], [5, 'No'], [7, 'V']]
+    elements = list(load_elements('PeriodicTableJSON_lite.json').keys())
+    element_entries = []
+    for element in elements:
+        position = name.lower().find(element.lower())
+        if position != -1:
+            element_entries.append([position, element])
+    return sorted(element_entries)
 
 
 def choose_best(vars_dict):
